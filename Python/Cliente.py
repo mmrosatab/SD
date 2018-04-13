055f90a42fc3e673f61931cec154513af424800e
ThiagoFrazao <thiago_frasao@hotmail.com.br>
2018-04-13 18:05:00 -0300

    Menu cliente criado

---
+++ Python/Cliente.py
@@ -0,0 +1,97 @@
+
+import xmlrpc.client
+
+def main():
+    
+    print("Conectando ao servidor...")
+   
+    cliente = xmlrpc.client.ServerProxy("http://localhost:8080/")
+
+    consultando = True
+    while consultando:
+        print("Digite o numero da operacao que deseja realizar:")
+        print("1. Cadastrar Nota")
+        print("2. Consulta nota de um aluno em uma disciplina especifica")
+        print("3. Consultar todas as notas de um aluno")
+        print("4. Calcular CR de um aluno")
+        print("5. Sair")
+        opcao = input()       
+   
+        if opcao == "1":            
+            matricula  = input("Matricula do aluno:")
+            disciplina = input("Digite o codigo da disciplina:")
+            
+            while len(disciplina) > 4:
+                print("\nCodigo de disciplina incorreto. Digite novamente.\n")
+                disciplina = input("Digite o codigo da disciplina:")
+                
+            nota       = input("Digite a nota a ser cadastrada:")                       
+            
+            print(cliente.cadastrarNota(int(matricula),disciplina,float(nota)))
+            
+            decisao = input("Caso  deseje fazer outra operacao digite: Y.\n")            
+            if decisao != "Y":
+                consultando = False
+                
+        elif opcao == "2":
+            matricula  = input("Matricula do aluno:")
+            disciplina = input("Código da disciplina")
+            
+            while len(disciplina) > 4:
+                print("\nCodigo de disciplina incorreto. Digite novamente.\n")
+                disciplina = input("Digite o codigo da disciplina:")
+            
+            retorno = cliente.consultarNota(int(matricula),disciplina)
+            if retorno == False:
+                print("ERRO: A nota requisitada nao existe no sistema.")               
+            else:
+                print(retorno)
+            
+            decisao = input("Caso  deseje fazer outra operacao digite: Y.\n")                
+            if decisao != "Y":
+                consultando = False    
+            
+        
+        elif opcao == "3":
+            matricula = input("Matricula do aluno:")
+            
+            retorno = cliente.listarNotas(int(matricula))
+            
+            if retorno == False:
+                print("ERRO: A matricula indicada nao existe ou nao possui notas cadastradas!")            
+            else:
+                print(retorno)
+            
+            decisao = input("Caso  deseje fazer outra operacao digite: Y.\n")                
+            if decisao != "Y":
+                consultando = False
+            
+        elif opcao == "4":
+            matricula = input("Matrica do aluno:")
+            
+            retorno = cliente.consultarCR(int(matricula))
+            
+            if retorno == False:
+                print("ERRO: A matricula indicada nao existe ou nao possui notas cadastradas!")
+            else:
+                print(retorno)
+            
+            decisao = input("Caso  deseje fazer outra operacao digite: Y.\n")                
+            if decisao != "Y":
+                consultando = False
+                
+        elif opcao == "5":
+            consultando = False
+        else:
+            print("Opcao invalida. Tente novamente.")
+        
+            
+    
+    cliente.terminar()
+                                    
+    
+    
+    
+if __name__ == "__main__":
+    main()
+
---
+++ Python/Servidor.py
@@ -0,0 +1,118 @@
+
+
+
+from xmlrpc.server import SimpleXMLRPCServer
+import psycopg2
+
+class Servidor():
+ 
+    def __init__(self):
+        
+        self.con = psycopg2.connect(host='localhost', database='Historico', user='postgres', password='thi30031993thi')
+        cur = self.con.cursor()
+        
+        sql = "select exists (select 1 from information_schema.tables where table_name = 'periodo');"
+        cur.execute(sql)
+        retorno = cur.fetchone()
+        
+        if retorno == "False":
+            sql = "CREATE TABLE Periodo ( matricula int NOT NULL, disciplina varchar(4)",
+            "NOT NULL, nota float NOT NULL, primary key(matricula, disciplina));"
+            cur.execute(sql)
+                   
+        
+        self.con.commit()
+        
+        self.server = SimpleXMLRPCServer(("localhost", 8080))
+        self.server.register_function(self.cadastrarNota)
+        self.server.register_function(self.consultarNota)
+        self.server.register_function(self.listarNotas)
+        self.server.register_function(self.consultarCR)
+        self.server.register_function(self.terminar)
+        
+        self.quit = False
+        
+        while not self.quit:
+        	    self.server.handle_request()
+        
+
+    def cadastrarNota(self,mat,codDisc,nota):
+        cur   = self.con.cursor()
+        
+        sql   = 'SELECT nota FROM Periodo WHERE matricula = ' + str(mat) + ' AND disciplina = ' + "'" + codDisc + "'"
+        cur.execute(sql)
+        
+        if cur.rowcount > 0:
+            sql = 'UPDATE Periodo SET nota = '+ str(nota) + ' WHERE matricula = ' + str(mat) + ' AND disciplina = ' + "'" + codDisc + "'"
+        
+        else:
+            sql = 'INSERT INTO Periodo VALUES ('+ str(mat) + ",'" + codDisc + "'," + str(nota) + ');'
+            
+        cur.execute(sql)
+        self.con.commit()
+        return "Nota cadastrada."
+    
+    def consultarNota(self,matricula, disciplina):
+        cur      = self.con.cursor()
+        sql      = 'SELECT disciplina, nota FROM Periodo WHERE matricula = '+ str(matricula) + ' AND disciplina = ' + "'" + disciplina + "';"
+        cur.execute(sql)        
+        linha    = cur.fetchone()
+        if linha is None:   
+            return False         
+        else:                  
+            return linha[0] + ' ' + str(linha[1])
+            
+        
+    
+    def listarNotas(self,matricula):
+        cur      = self.con.cursor()
+        sql      = 'SELECT disciplina, nota FROM Periodo WHERE matricula = '+ str(matricula);
+        cur.execute(sql)
+        linhas   = cur.fetchall()
+        result   = ''
+        if len(linhas) == 0:           
+            return False
+        
+        else:
+            
+            for linha in linhas:
+                result = result + linha[0] + ' ' + str(linha[1]) + '\n'
+                
+            return result
+            
+    
+    def consultarCR(self,matricula):
+        cur      = self.con.cursor()
+        sql      = 'SELECT nota FROM Periodo WHERE matricula = '+ str(matricula)
+        cur.execute(sql)
+        notas    = cur.fetchall()
+        if len(notas) == 0:            
+            return False
+        else:
+            
+            cr = 0
+            
+            for nota in notas:
+                cr = cr + float(nota[0]) 
+            
+            return round(cr/cur.rowcount,2)
+
+    def terminar(self):        
+        self.quit = True
+        return True
+    
+    
+
+def main():
+    
+    print("Inicializando servidor:")
+    Servidor()
+    print("Servidor finalizado!")
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
